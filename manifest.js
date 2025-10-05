20f15dd785302369c94e499ca062743f6ef6e318
chaosmethod <60120308+chaosmethod@users.noreply.github.com>
2025-10-04 23:38:07 -0500

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,8 @@
+{
+  "title": "Rabbit Pal",
+  "url": "https://rabbit-pal-b91ea282.base44.app/",
+  "description": "Your virtual Tamagotchi pet on the Rabbit R1",
+  "iconUrl": "https://raw.githubusercontent.com/chaosmethod/rabbit-pal/main/icon.png",
+  "themeColor": "#FFB6C1"
+}
+
