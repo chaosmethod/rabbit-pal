80c637f27e7df34d0d9be367b170b55f6409f2cc
chaosmethod <60120308+chaosmethod@users.noreply.github.com>
2025-10-04 23:48:55 -0500

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -3,6 +3,7 @@
   "url": "https://rabbit-pal-b91ea282.base44.app/",
   "description": "Your virtual Tamagotchi pet on the Rabbit R1",
   "iconUrl": "https://raw.githubusercontent.com/chaosmethod/rabbit-pal/main/icon.png",
-  "themeColor": "#FFB6C1"
+  "themeColor": "#F0EAD6"
 }
+
 
